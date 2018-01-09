c79953c4a326b385167654412f616ee1e4049094
fajarachmadyusup13 <fajarachmady13@gmail.com>
2018-01-09 13:30:36 +0700

    hooked up to reducer_post

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,6 +4,9 @@ export default function (state=[], action) {
     
     switch (action.type) {
         case FETCH_WEATHER:
+            console.log(action.payload);
+            
+
             return [action.payload.data, ...state];
     }
     return state;
